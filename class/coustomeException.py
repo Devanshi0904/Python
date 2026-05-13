c8f4acfde8f80a91b8339c9692379ccdf7799aa7
devanshi <laddevanshi00@gmail.com>
2026-05-13 11:52:04 +0530

    add

--- class/coustomeException.py
+++ class/coustomeException.py
@@ -16,33 +16,55 @@
 
 
 
-class InsufficintAmountException(Exception):
-    def __init__(self, msg):
-        super().__init__(msg)
+import random
 
-class bank :
-    balance = 0
+# Input from user
+first_name = input("Enter First Name: ")
+last_name = input("Enter Last Name: ")
 
-    def check_balance(self):
-        print("current balance is :",self.balance)
+# Validation
+if len(first_name) < 2 or len(last_name) < 2:
+    print("Please enter at least 2 characters in both names.")
+else:
+    part1 = first_name[:2]
+    part2 = last_name[:2]
+
+    random_number = random.randint(1000, 9999)
+
+    password = part1 + part2 + str(random_number)
+
+    print("Generated Password:", password)
+
+
+
+
+# class InsufficintAmountException(Exception):
+#     def __init__(self, msg):
+#         super().__init__(msg)
+
+# class bank :
+#     balance = 0
+
+#     def check_balance(self):
+#         print("current balance is :",self.balance)
     
-    def deposite(self,amt):
-        self.balance+=amt
+#     def deposite(self,amt):
+#         self.balance+=amt
 
-    def withdrow(self,amt):
-        if amt > self.balance:
-            raise InsufficintAmountException(f"you need more {amt-self.balance} in your account to withrow")
-        else:
-            self.balance-=amt
-b = bank()
-b.check_balance()
-b.deposite(80000)
-b.check_balance()
-try :
-    b.withdrow(200000)
-except Exception as e:
-    print(e)
-b.check_balance()
+#     def withdrow(self,amt):
+#         if amt > self.balance:
+#             raise InsufficintAmountException(f"you need more {amt-self.balance} in your account to withrow")
+#         else:
+#             self.balance-=amt
+# b = bank()
+# b.check_balance()
+# b.deposite(80000)
+# b.check_balance()
+# try :
+#     b.withdrow(200000)
+# except Exception as e:
+#     print(e)
+# b.check_balance()
 
 
 
